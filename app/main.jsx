1b62ab55ac413386c3628c94b389c6def2ff636a
yuanzm <1229084233@qq.com>
2016-07-10 20:57:15 +0800

    init babel & webpack

---
+++ app/main.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const a = 1;
+let b = Array.of(3);
+
+var c = [1, 2 ,3 , 4];
+
+var d = c.find((n) => n > 2);
+
+console.log(d);
+
+c.includes(3);
+
+console.log(...[1,2])
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, 'app/main.jsx')
+    ],
+    output: {
+        path: __dirname + '/build',
+        publicPath: '/',
+        filename: './bundle.js'
+    },
+    module: {
+        loaders:[
+            { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
+            { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    }
+};
